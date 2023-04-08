2772fe7cd690ad20b8f67293aefc34ec5f6477d1
andre souza <andre.souza@creathus.org.br>
2023-04-08 06:48:30 -0400

    feat: add a regex for search a params in routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from "./database.js";
 import { randomUUID } from "node:crypto";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handle: (request, response) => {
       const users = database.select('users')
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handle: (request, response) => {
       const {name, email} = request.body
     
@@ -29,5 +30,17 @@ export const routes = [
 
       return response.writeHead(201).end();
     }
-  }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handle: (request, response) => {
+      return response.end();
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handle: (request, response) => {}
+  },
 ]
--- src/server.js
+++ src/server.js
@@ -2,14 +2,30 @@ import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
 
+/**
+ * Query parameters: URL stateful -> Filtros, paginação, não-obrigatórios 
+ *  --> http://localhost:3333/users?name=Andre&userId=123
+ * 
+ * Route parameters: Identifiação de recurso:
+ *    GET http://localhost:3333/users/1 
+ *    DELETE http://localhost:3333/users/1 
+ * 
+ * Request body: Envio de informações via formulário (HTTPs)
+ * 
+ * 
+*/
+
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
   
   await json(request, response)
 
-  const route = routes.find(route => route.method === method && route.path === url);
-  if(route)
+  const route = routes.find(route => route.method === method && route.path.test(url));
+  if(route) {
+    const routeParams = request.url.match(route.path)
+    console.log(routeParams)
     return route.handle(request, response)
+  }
 
   response.writeHead(404).end("Rota não encontrada!");
 });
